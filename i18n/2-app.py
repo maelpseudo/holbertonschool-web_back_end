b8cf04c41946ebe7d41e3f65f1a5d8132e50d100
Maël PRO <maelpro@MacBook-Air-de-Mael.local>
2025-07-25 12:08:47 +0200

    Added i18n and unittests

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -13,10 +13,9 @@ class Config(object):
 
 app = Flask(__name__)
 app.config.from_object(Config)
-babel = Babel(app)
+babel = Babel()
 
 
-@babel.localeselector
 def get_locale():
     """ Locale language
 
@@ -26,6 +25,9 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
+babel.init_app(app, locale_selector=get_locale)
+
+
 @app.route('/', strict_slashes=False)
 def hello_world():
     """ Greeting
@@ -33,7 +35,7 @@ def hello_world():
         Return:
             Initial template html
     """
-    return render_template('1-index.html')
+    return render_template('2-index.html')  # Attention au bon nom de fichier HTML ici
 
 
 if __name__ == "__main__":
