63e32bc407c47012ce5a3afcbf729776b8d51ca0
Maël PRO <maelpro@MacBook-Air-de-Mael.local>
2025-07-25 12:09:45 +0200

    Added i18n and unittests

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -22,7 +22,9 @@ def get_locale():
         Return:
             Best match to the language
     """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(
+        app.config['LANGUAGES']
+    )
 
 
 babel.init_app(app, locale_selector=get_locale)
