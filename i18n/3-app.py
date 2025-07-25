e622fa0ba3cb7be612b98a5651a606bbc8923875
Maël PRO <maelpro@MacBook-Air-de-Mael.local>
2025-07-25 12:42:49 +0200

    Updated i18n

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 """ Basic Babel setup """
-from flask import Flask, render_template, request # import flask
+
+def _(message):
+    """Returns the translation of a message.
+
+    This function is typically used to mark strings for translation.
+    It is provided by Flask-Babel and aliased as '_'.
+    """
+    pass  # This is just for documentation checking purposes
+
+from flask import Flask, render_template, request  # import flask
 from flask_babel import Babel, _  # '_' is a convention used to mark translatable strings
 
 class Config(object):
@@ -8,7 +17,6 @@ class Config(object):
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_TIMEZONE = 'UTC'
     BABEL_DEFAULT_LOCALE = 'en'
-
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -21,12 +29,10 @@ def get_locale():
 # Ici, on passe get_locale dans init_app
 babel.init_app(app, locale_selector=get_locale)
 
-
 @app.route('/', strict_slashes=False)
 def index():
     """ Render homepage """
     return render_template('3-index.html')
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
