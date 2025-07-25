03ded092d23e6ea3937880c5e81ba52c0496cc0e
Maël PRO <maelpro@MacBook-Air-de-Mael.local>
2025-07-25 12:50:15 +0200

    Updated i18n

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Basic Babel setup """
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel, _ # Function _
 
 
 class Config(object):
